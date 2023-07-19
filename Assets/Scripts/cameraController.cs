0032369e249c969db091428094cc085542597a1d
Zandar Beker <zanbek56@kingshigh.school.nz>
2023-07-19 13:25:36 +1200

    Finalising First Level
    Nearly done with everything relevent to the first level/map.

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    public GameObject Player ; 
+    public GameObject child ; 
+    public float speed;
+
+    private void Start() {
+        Player = GameObject.FindGameObjectWithTag("CameraTarget");
+        child = Player.transform.Find("CameraConstraint").gameObject;
+        
+    }
+   
+    private void FixedUpdate() {
+    
+        follow();
+
+    }
+
+    private void follow() {
+
+        gameObject.transform.position = Vector3.Lerp(transform.position,child.transform.position,Time.deltaTime * speed);
+        gameObject.transform.LookAt(Player.gameObject.transform.position);
+
+    }
+
+}
